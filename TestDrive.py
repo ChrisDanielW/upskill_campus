6dd1951e26d938caa53cb9fbc28822cb6a894e9a
ChrisDanielW <danielwchris@gmail.com>
2023-07-03 16:18:32 +0530

    demo UI code + some more changes

--- TestDrive.py
+++ TestDrive.py
@@ -10,24 +10,32 @@ class App(ctk.CTk):
         self.geometry("640x270")
 
         def button_pressed():
+            # try:
             self.slink.set(Sh().tinyurl.short(blink.get()))
+            #     self.disclaimer.set('')
+            # except:
+            #     self.disclaimer.set('Invalid URL')
 
-        self.l_1 = ctk.CTkLabel(self, text="Enter a link to shorten:")
-        self.l_1.pack(padx=10, pady=10)
+        self.label1 = ctk.CTkLabel(self, text="Enter a link to shorten:")
+        self.label1.pack(padx=10, pady=10)
 
         blink = ctk.StringVar()
-        self.b_blink = ctk.CTkEntry(self, width=450, height=28, textvariable=blink)
-        self.b_blink.pack()
+        self.blink_entry = ctk.CTkEntry(self, width=450, height=28, textvariable=blink)
+        self.blink_entry.pack()
 
-        self.l_2 = ctk.CTkLabel(self, text="Shortened URL:")
-        self.l_2.pack(padx=10, pady=10)
+        self.label2 = ctk.CTkLabel(self, text="Shortened URL:")
+        self.label2.pack(padx=10, pady=10)
 
         self.slink = ctk.StringVar()
-        self.b_slink = ctk.CTkEntry(self, width=350, height=28, textvariable=self.slink)
-        self.b_slink.pack()
+        self.slink_entry = ctk.CTkEntry(self, width=350, height=28, textvariable=self.slink)
+        self.slink_entry.pack()
 
-        self.button = ctk.CTkButton(self, text="Shorten URL", command=button_pressed)
-        self.button.pack(padx=20, pady=20)
+        self.shorts_button = ctk.CTkButton(self, text="Shorten URL", command=button_pressed)
+        self.shorts_button.pack(padx=20, pady=20)
+
+        self.disclaimer = ctk.StringVar()
+        self.disclaimer_label = ctk.CTkLabel(self, textvariable=self.disclaimer)
+        self.disclaimer_label.pack(padx=10, pady=10)
         # self.grid_columnconfigure(0, weight=1)
         # self.button.grid(row=0, column=0, padx=20, pady=20, sticky="e")
 
